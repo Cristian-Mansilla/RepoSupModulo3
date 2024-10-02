fa173f53a046a7ff814fc1d5eaee77bc1afc1608
cristian <cristian-mansilla@live.com.ar>
2024-10-02 01:02:08 -0300

    eslint corriendo bajo comando

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,33 +1,7 @@
-interface IAddress {
-  street: string;
-  city: string;
-}
+var nombre = "cris";
 
-enum UserRole {
-  admin = "admin",
-  user = "user",
-  guest = "guest"
-}
-
-type TUser = {
-  name: string;
-  age: number;
-  active: boolean;
-  address: IAddress;
-  role: UserRole;
+function miFuncion() {
+  return 2 + 3;
 };
 
-///////////////////////////
-
-const user: TUser = {
-    name: "Cristian",
-    age: 33,
-    active: true,
-    address: {
-        street: "calle Falsa",
-        city: "Cordoba"
-    },
-    role: UserRole.admin
-}
-
-console.log(user);
+console.log(nombre)
