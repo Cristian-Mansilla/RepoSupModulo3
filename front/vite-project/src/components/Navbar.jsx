a79613bfd2cf232f5c4065722d99be757cc62280
cristian <cristian-mansilla@live.com.ar>
2024-10-24 02:12:40 -0300

    front con Vite

---
+++ front/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import  style  from "../styles/Navbar.module.css"
+
+export const Navbar = () => {
+  return <div className={style.container}>
+    <a href="">Home</a>
+    <a href="">Mis Turnos</a>
+    <a href="">Contacto</a>
+    <a href="">About</a>
+  </div>
+};
